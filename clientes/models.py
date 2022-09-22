7f1db3910ca8d7bff57b13e9e67a275ab511b2cf
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-22 22:34:19 +0100

    estrutura do banco completa

--- clientes/models.py
+++ clientes/models.py
@@ -1,5 +1,3 @@
-import email
-from mailbox import NotEmptyError
 from django.db import models
 
 # Create your models here.
@@ -16,3 +14,13 @@ class Carro(models.Model):
     carros = models.CharField(max_length=20)
     placas = models.CharField(max_length=20)
     ano = models.CharField(max_length=10)
+
+    # Criando uma relação de um pra muitos
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+
+    # bonificação apos 10 lavagens ou concertos
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.carros
