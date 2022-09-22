6c16efef34c03a38680befc566affd0a731199a0
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-22 01:01:37 +0100

    criando a base para salvar no db

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,21 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        
+        #pegando uma lista
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        ano = request.POST.getlist('ano')
+        print(carros)
+        print(placas)
+        print(ano)
