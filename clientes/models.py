73c72be807c7d3e34290428b7fbe124e59df55d7
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-22 21:35:17 +0100

    criamos os dados para salvar os dados do cliente

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,13 @@
+import email
+from mailbox import NotEmptyError
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=12)
+
+    def __str__(self) -> str:
+        return self.nome
